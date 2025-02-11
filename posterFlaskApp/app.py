d712dffae609115bd01bee014e9d3819b333df7b
Rangel Koli <77476841+rangelkoli@users.noreply.github.com>
2025-02-11 13:01:40 -0500

    Update app.run to bind to all interfaces

--- posterFlaskApp/app.py
+++ posterFlaskApp/app.py
@@ -460,4 +460,4 @@ def scoring_posters():
 
 
 if __name__ == '__main__':
-    app.run(debug=os.getenv('FLASK_DEBUG', 'False').lower() == 'true', port=5000)
+    app.run(debug=os.getenv('FLASK_DEBUG', 'False').lower() == 'true', port=5000, host="0.0.0.0")
